ab6d01fc32665911daf85f722d7ffe8079f3bc57
lijiahong <lijiahong@meituan.com>
2019-05-14 14:46:03 +0800

    minor updates

--- avod/experiments/mpi_run_training.sh
+++ avod/experiments/mpi_run_training.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-
-export PYTHONPATH=":/chenzhuo/heterofusion:/chenzhuo/heterofusion/wavedata"
-export CUDA_VISIBLE_DEVICES="4,5,6,7"
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 gpu_num=4
 
 mpirun --allow-run-as-root -np ${gpu_num} -H localhost:${gpu_num} \
         -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
         -mca pml ob1 -mca btl ^openib \
-        python run_training.py --pipeline_config=../configs/rpn_cars_alt_1.config
+        python run_training.py --pipeline_config=$1 --data_split='train'
