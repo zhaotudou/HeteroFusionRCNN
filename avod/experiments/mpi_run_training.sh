e12047cd4bbadbd8bc6c7718b56a5a2fe686c465
lijiahong <lijiahong@meituan.com>
2019-05-14 16:52:31 +0800

    minor fix

--- avod/experiments/mpi_run_training.sh
+++ avod/experiments/mpi_run_training.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES="0,1,2,3"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gpu_num=4
 
 mpirun --allow-run-as-root -np ${gpu_num} -H localhost:${gpu_num} \
         -bind-to none -map-by slot -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH \
         -mca pml ob1 -mca btl ^openib \
-        python run_training.py --pipeline_config=$1 --data_split='train'
+        python $DIR/run_training.py --pipeline_config=$1 --data_split='train'
